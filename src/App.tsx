c1bd8bc1e2aa1b37d03209b3c100fc8ff7aa8aeb
Ezequiel Gonzalez <e.gonzalez95@hotmail.com>
2024-09-23 13:57:19 -0300

    feat: Modified and andded configuration for styles, project and types.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/ui/navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import { AuthProvider } from "./contexts/AuthContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ProtectedRoute from "./components/auth/protected-route";
+
+const queryClinet = new QueryClient();
 
 function App() {
-  const [count, setCount] = useState(0)
+	return (
+		<main className="h-full min-h-screen bg-background text-foreground p-10">
+			<QueryClientProvider client={queryClinet}>
+				<BrowserRouter>
+					<AuthProvider>
+						<Navbar />
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+						<Routes>
+							<Route
+								path="/"
+								element={
+									<ProtectedRoute>
+										<Home />
+									</ProtectedRoute>
+								}
+							/>
+							<Route path="/login" element={<Login />} />
+						</Routes>
+					</AuthProvider>
+				</BrowserRouter>
+			</QueryClientProvider>
+		</main>
+	);
 }
 
-export default App
+export default App;
